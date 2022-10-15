7b704c826050962a75b5188b4f361a37ba42c33e
Ryotaro Hada <67140275+ryotarohada@users.noreply.github.com>
2022-10-15 20:48:37 +0900

    Update pointer.rs

--- src/pointer.rs
+++ src/pointer.rs
@@ -2,6 +2,17 @@ pub fn run() {
   println!("Here is pointer module!!");
 
   let value = 1;
+  let (name, age) = get_user();
 
   println!("Value address is {:p}", &value);
+  println!("User data : name={} age={}", name, age);
+}
+
+fn get_user() -> (&'static str, i32) {
+  let user = ("foo", 250);
+  return user
+}
+
+struct User<T> {
+  name: T
 }
